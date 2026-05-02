c4b124f16ab01c911e94842a8838511f6dc4dfb9
Anirudha Talmale <anirudhatalmale6@gmail.com>
2026-05-02 07:06:46 +0000

    Rename Object.php to CakeObject.php for autoloader compatibility

--- core/lib/Cake/bootstrap.php
+++ core/lib/Cake/bootstrap.php
@@ -136,7 +136,7 @@ App::uses('ErrorHandler', 'Error');
 App::uses('Configure', 'Core');
 App::uses('CakePlugin', 'Core');
 App::uses('Cache', 'Cache');
-App::uses('Object', 'Core');
+App::uses('CakeObject', 'Core');
 App::$bootstrapping = true;
 
 Configure::bootstrap(isset($boot) ? $boot : true);
